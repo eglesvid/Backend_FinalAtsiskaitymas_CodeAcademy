f562e707eb14be8a022e82f920a1dca2bd111d2c
eglesvid <eglesvid@gmail.com>
2023-05-15 22:47:36 +0300

    updated because of vercel

--- server.js
+++ server.js
@@ -12,14 +12,11 @@ const MONGO_URI = process.env.MONGO_URI;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
 app.use(router);
 
 mongoose
   .connect(MONGO_URI)
   .then(() => console.log("connected"))
   .catch((err) => console.log(err));
-
-// app.listen(PORT, () => console.log(`application running on port ${PORT}`));
 
 export default app;
